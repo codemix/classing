c746e5ca2857984ab438f1c6da188e7ff5b206c7
Charles Pick <charles.pick@gmail.com>
2014-05-28 16:24:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,146 @@
+# Classer
+
+Easy, flexible classes for JavaScript, works in node and all modern browser (> IE8).
+
+
+# Installation
+
+Via [npm](https://npmjs.org/package/classer):
+
+    npm install --save classer
+
+
+or [bower](http://bower.io/search/?q=classer):
+
+
+    bower install --save classer
+
+
+
+# Usage
+
+**Simple classes**
+
+```js
+var Class = require('classer');
+
+var Person = Class({
+  name: {
+    value: 'No Name'
+  },
+  dateOfBirth: {}
+});
+
+var person = new Person();
+
+person.name === 'No Name'; // => true
+
+var person = new Person({
+  name: 'Bob',
+  dateOfBirth = new Date()
+});
+```
+
+
+**Inheritance**
+
+```js
+var Vehicle = Class({
+  name: {
+    value: 'No Name'
+  }
+});
+
+var RoadVehicle = Vehicle.extend({
+  wheels: {
+    value: 0
+  },
+  capacity: {
+    value: 0
+  },
+  capacityPerWheel: {
+    get: function () {
+      return (this.capacity || 1) / (this.wheels || 1)
+    }
+  }
+});
+
+var Car = RoadVehicle.extend({
+  wheels: {
+    value: 4
+  }
+});
+
+var mini = new Car({
+  name: 'mini',
+  capacity: 3.5
+});
+
+console.log(mini.capacityPerWheel);
+
+```
+
+
+**Mixins**
+
+```js
+
+var Truck = RoadVehicle.extend();
+
+Truck.mixin({
+  capacity: 2,
+  wheels: 8
+});
+
+var truck = new Truck();
+console.log(truck.capacityPerWheel);
+
+
+```
+
+
+**Meta programming**
+
+```js
+
+var AutoBinding = Class();
+
+Object.defineProperty(AutoBinding, 'defineProperty', {
+  value: function (name, descriptor) {
+    descriptor = descriptor || {};
+    if (typeof descriptor.value === 'function') {
+      descriptor.value = descriptor.value.bind(this);
+    }
+    Class.defineProperty.call(this, name, descriptor);
+  }
+});
+
+
+var MyConsole = AutoBinding.extend({
+  log: {
+    value: function (message) {
+      this.console.log('[' + new Date() + ']', message);
+    }
+  },
+  console: {
+    value: console
+  }
+});
+
+var myconsole = new MyConsole(),
+    log = myconsole.log;
+
+log('Hello');
+log('World');
+
+```
+
+# Running the tests
+
+First, `npm install`, then `npm test`. Code coverage generated with `npm run coverage`.
+
+
+# License
+
+MIT, see [LICENSE.md](LICENSE.md).
+
---
+++ test/test.js
@@ -0,0 +1,348 @@
+var Class = require('../classer'),
+    expect = require('expect.js');
+
+describe('Class', function () {
+
+  var Base = Class();
+
+  var Thing = Class({
+    name: {
+      value: 'Unnamed Item'
+    },
+    className: function () {
+      return 'Thing';
+    },
+    computed: {
+      get: function () {
+        return Date.now();
+      }
+    }
+  });
+
+  Thing.inherits(Base);
+
+  Thing.staticMethod = function () { return 'Thing'; };
+
+
+  var Person = Class({
+    name: {
+      value: 'Unnamed Person'
+    },
+    age: {},
+    location: {
+      value: 'The world.'
+    },
+    className: function () {
+      return 'Person';
+    }
+  });
+
+
+  it('should add properties correctly', function () {
+    var thing = new Thing();
+    thing.name.should.equal('Unnamed Item');
+    thing.className.should.have.type('function');
+    thing.className().should.equal('Thing');
+  });
+
+  describe('Class.defineProperty()', function () {
+    it('should add new properties correctly', function () {
+      Thing.defineProperty('url', {
+        value: 'http://example.com/'
+      });
+      var thing = new Thing();
+      thing.name.should.equal('Unnamed Item');
+      thing.url.should.equal('http://example.com/');
+    });
+
+    it('should configure objects correctly', function () {
+      var thing = new Thing({
+        name: 'codemix',
+        url: 'http://codemix.com/'
+      });
+
+      thing.name.should.equal('codemix');
+      thing.url.should.equal('http://codemix.com/');
+    });
+
+    it('should add a property without a descriptor', function () {
+      Thing.defineProperty('sameAs');
+    });
+  });
+
+  describe('Class.defineProperties()', function () {
+    it('should define multiple properties at once', function () {
+      Thing.defineProperties({
+        description: {
+          value: 'No description yet.'
+        },
+        alternateName: {}
+      });
+
+      var thing = new Thing();
+      thing.should.have.properties(['name', 'description', 'url', 'alternateName']);
+    });
+
+    it('should do nothing if no properties are specified', function () {
+      Thing.defineProperties();
+    });
+  });
+
+  describe('Class::constructor()', function () {
+    it('should use the correct context, if `new` is not used', function () {
+      var thing = Thing();
+      thing.should.be.an.instanceOf(Thing);
+    });
+  });
+
+  describe('Class::configure()', function () {
+    var MyClass = Thing.extend({
+      setter: {
+        get: function () {
+          return this._setter;
+        },
+        set: function (setter) {
+          this._setter = setter;
+        }
+      }
+    })
+    it('configure an object correctly', function () {
+      var obj = new MyClass();
+      obj.configure({
+        name: 'myname',
+        setter: 'wat'
+      });
+
+      obj.should.have.properties({
+        name: 'myname',
+        setter: 'wat'
+      });
+    });
+  });
+
+
+  describe('Class.create()', function () {
+    it('should create a new class instance', function () {
+      var thing = Thing.create();
+      thing.should.be.an.instanceOf(Thing);
+    });
+  });
+
+
+  describe('Class.inherits()', function () {
+    it('should inherit from other classes', function () {
+      Person.inherits(Thing);
+
+      var person = new Person();
+
+      person.should.be.an.instanceOf(Person);
+      person.should.be.an.instanceOf(Thing);
+      person.should.have.properties(['name', 'description', 'url', 'alternateName', 'age', 'location']);
+    });
+    it('should copy static values', function () {
+      Person.should.have.property('staticMethod');
+      Person.staticMethod().should.equal('Thing');
+    });
+    it('should apply the default values', function () {
+      var person = new Person();
+      person.should.have.properties({
+        name: 'Unnamed Person',
+        description: 'No description yet.',
+        url: 'http://example.com/',
+        alternateName: undefined,
+        age: undefined,
+        location: 'The world.'
+      });
+    });
+    it('should configure the child class correctly', function () {
+      var person = new Person({
+        name: 'Charles',
+        description: 'Hello World!',
+        url: 'http://codemix.com/',
+        age: 29,
+        location: 'United Kingdom'
+      });
+
+      person.should.have.properties({
+        name: 'Charles',
+        description: 'Hello World!',
+        url: 'http://codemix.com/',
+        age: 29,
+        location: 'United Kingdom'
+      });
+    });
+    it('should inherit from other classes, deeply', function () {
+      var Visitor = Class({
+        name: {
+          value: 'Unnamed Visitor'
+        },
+        visitedAt: {},
+        className: function () {
+          return 'Visitor'
+        }
+      });
+
+      Visitor.inherits(Person);
+
+      Visitor.should.have.property('staticMethod');
+      Visitor.staticMethod().should.equal('Thing');
+
+      var visitor = new Visitor();
+      visitor.should.be.an.instanceOf(Visitor);
+      visitor.should.be.an.instanceOf(Person);
+      visitor.should.be.an.instanceOf(Thing);
+      visitor.should.have.properties(['name', 'description', 'url', 'alternateName', 'age', 'location', 'visitedAt']);
+      visitor.className().should.equal('Visitor');
+    });
+
+    it('should inherit from native objects', function () {
+      var MyError = Class.create();
+      MyError.inherits(Error);
+
+      var err = new MyError();
+      err.should.be.an.instanceOf(MyError);
+      err.should.be.an.instanceOf(Error);
+    });
+
+    it('should not overwrite custom `configure()` and `toJSON()` functions', function () {
+      var MyClass = Thing.extend();
+
+      MyClass.staticMethod = function () { return 'MyClass'; };
+
+      MyClass.defineProperties({
+        configure: function (config) {
+          this.bananas = true;
+        },
+        toJSON: function () {
+          return false;
+        }
+      });
+
+      MyClass.defineProperty('foo');
+
+      var instance = new MyClass({
+        name: 'Test'
+      });
+
+      instance.name.should.not.equal('Test');
+      instance.bananas.should.be.true;
+
+
+      expect(instance.configure.isAutoGenerated).to.be.undefined;
+      expect(instance.toJSON.isAutoGenerated).to.be.undefined;
+    });
+
+    it('should not overwrite custom `configure()` and `toJSON()` functions, when deeply inheriting', function () {
+      var MyClass = Thing.extend({
+        configure: function (config) {
+          this.bananas = true;
+        },
+        toJSON: function () {
+          return false;
+        }
+      });
+
+      MyClass.staticMethod = function () { return 'MyClass'; };
+
+      var MyChildClass = Class();
+      MyChildClass.staticMethod = function () { return 'MyClass'; };
+      MyChildClass.inherits(MyClass);
+
+
+      var instance = new MyChildClass({
+        name: 'Test'
+      });
+
+      instance.name.should.not.equal('Test');
+      instance.bananas.should.be.true;
+
+
+      expect(instance.configure.isAutoGenerated).to.be.undefined;
+      expect(instance.toJSON.isAutoGenerated).to.be.undefined;
+    });
+  });
+
+  describe('Class.extend()', function () {
+
+
+    var Visitor = Person.extend({
+      name: {
+        value: 'Unnamed Visitor'
+      },
+      visitedAt: {},
+      className: function () {
+        return 'Visitor'
+      },
+      parentClass: function () {
+        return this.super.className();
+      }
+    });
+
+
+
+    it('should inherit from other classes', function () {
+      Visitor.inherits(Person);
+
+      var visitor = new Visitor();
+
+      visitor.should.be.an.instanceOf(Visitor);
+      visitor.should.be.an.instanceOf(Person);
+      visitor.should.be.an.instanceOf(Thing);
+      visitor.should.have.properties(['name', 'description', 'url', 'alternateName', 'age', 'location']);
+      visitor.className().should.equal('Visitor');
+    });
+
+    it('should apply the default values', function () {
+      var visitor = new Visitor();
+      visitor.should.have.properties({
+        name: 'Unnamed Visitor',
+        description: 'No description yet.',
+        url: 'http://example.com/',
+        alternateName: undefined,
+        age: undefined,
+        location: 'The world.',
+        visitedAt: undefined
+      });
+    });
+    it('should configure the child class correctly', function () {
+      var now = new Date();
+      var visitor = new Visitor({
+        name: 'Charles',
+        description: 'Hello World!',
+        url: 'http://codemix.com/',
+        age: 29,
+        location: 'United Kingdom',
+        visitedAt: now
+      });
+
+      visitor.should.have.properties({
+        name: 'Charles',
+        description: 'Hello World!',
+        url: 'http://codemix.com/',
+        age: 29,
+        location: 'United Kingdom',
+        visitedAt: now
+      });
+    });
+
+    it('should populate `super`', function () {
+      var visitor = new Visitor();
+      visitor.parentClass().should.equal('Person');
+    });
+  });
+
+
+  describe('Class.mixin()', function () {
+    var MyClass = Thing.extend();
+
+    MyClass.mixin({
+      name: 'nope',
+      newProp: true
+    });
+
+    var obj = new MyClass();
+    obj.name.should.equal('nope');
+    obj.newProp.should.be.true;
+
+  });
+});
